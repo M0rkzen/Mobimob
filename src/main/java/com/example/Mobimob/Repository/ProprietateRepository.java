41584cc5406ca1fce54de6c3d3134ea2fd38629a
Marius Cota <113298614+M0rkzen@users.noreply.github.com>
2023-09-10 15:11:26 +0300

    Update ProprietateRepository.java

--- src/main/java/com/example/Mobimob/Repository/ProprietateRepository.java
+++ src/main/java/com/example/Mobimob/Repository/ProprietateRepository.java
@@ -3,8 +3,6 @@ package com.example.Mobimob.Repository;
 import com.example.Mobimob.Entity.Proprietate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.swing.*;
 import java.util.List;
 
 @Repository
